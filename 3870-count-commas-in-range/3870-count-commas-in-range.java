8120562497c25655a7228ad0c8d11875dd825f8f
Udhayakumar Mahendiran <udhayaak2830@gmail.com>
2026-09-08 11:38:57 +0530

    Time: 0 ms (100%), Space: 42.2 MB (93.72%) - LeetHub

---
+++ 3870-count-commas-in-range/3870-count-commas-in-range.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int countCommas(int num) {
+        int count = 0;
+        if(num < 1000){
+            return count;
+        }
+        if(num <= 100000){
+            count += num - 999;
+            
+            return count;
+        }
+        return count;
+    }
+}
